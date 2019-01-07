c471b924c54434c996688f6740414ef91b7ef80a
MarkFrance <markusfrance@hotmail.com>
2019-01-07 11:38:05 +0000

    Added betting constant variables, started place bet logic, added getDiceWinAmount function.

--- contracts/FaireumDice.sol
+++ contracts/FaireumDice.sol
@@ -1,8 +1,26 @@
 pragma solidity >=0.4.0 <0.6.0;
 
-// * Faireum Dice Game Contract
+// @title Faireum Dice Game Contract
 contract FaireumDice{
     
+    // State Variables
+    address constant TEST_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    uint constant MIN_BET = 0.01 ether;
+    uint constant MAX_AMOUNT = 300000 ether;
+
+    uint constant HOUSE_PERCENT = 1;
+    uint constant HOUSE_MINIMUM_AMOUNT = 0.0003 ether;
+
+    uint constant JACKPOT_MODULO = 1000;
+    uint constant JACKPOT_FEE = 0.001 ether;
+    uint constant MIN_JACKPOT_BET = 0.1 ether;
+
+    uint128 public lockedInBets;
+    address public owner;
+    address public croupier;
+
+    mapping (uint => Bet) bets;
+
     // A single bet struct
     struct Bet {
         uint amount;
@@ -12,20 +30,7 @@ contract FaireumDice{
         address user;
     }
 
-    mapping (uint => Bet) bets;
-
-    address constant TEST_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
-    uint128 public lockedInBets;
-    address public owner;
-    address public croupier;
-
-    // Constructor. 
-    constructor () public {
-        owner = msg.sender;
-        croupier = TEST_ADDRESS;
-    }
-
+    //Modifiers
     modifier onlyOwner {
         require (msg.sender == owner, "Must be contract owner to invoke this method.");
         _;
@@ -36,7 +41,13 @@ contract FaireumDice{
         _;
     }
 
-    // Fallback function
+    // Constructor. 
+    constructor () public {
+        owner = msg.sender;
+        croupier = TEST_ADDRESS;
+    }
+
+    // Fallback function. Deliberately left empty.
     function () public payable {
     }
 
@@ -50,8 +61,58 @@ contract FaireumDice{
         selfdestruct(owner);
     }
 
-    function placeBet() external payable {
+    // @dev Logic for placing a bet, recieves value of bet
+    // @param modulo - Modulo for the game
+    // @param commit - Unique bet identifier (Keccak256 hash)
+    function placeBet (
+        uint modulo, 
+        uint commit
+    ) 
+        external payable 
+    {
+        Bet storage bet = bets[commit];
+        require (bet.user == address(0), "Should be a new bet.");
+
+        //Validate amounts
+        uint amount = msg.value;
+        require (modulo > 1 && modulo <= 100, "Modulo is out of range.");
+        require (amount >= MIN_BET && amount <= MAX_AMOUNT, "Amount is out of range.");
+        
+        uint rollUnder;
+
         //TODO
+
+        //Store bet
+        bet.amount = amount;
+        bet.modulo = uint8(modulo);
+        bet.rollUnder = uint8(rollUnder);
+        bet.blockNumber = uint40(block.number);
+        bet.user = msg.sender;
+    }
+
+    // @dev returns the expected amount to win after subtracting house edge
+    // @param amount
+    // @param modulo
+    // @param rollUnder
+    function getDiceWinAmount(
+        uint amount, 
+        uint modulo, 
+        uint rollUnder
+    ) 
+        private 
+        pure 
+        returns (uint winAmount, uint jackpotFee) 
+    {
+       
+        jackpotFee = amount >= MIN_JACKPOT_BET ? JACKPOT_FEE : 0;
+
+        uint houseEdge = amount * HOUSE_PERCENT / 100;
+
+        if (houseEdge < HOUSE_MINIMUM_AMOUNT) {
+            houseEdge = HOUSE_MINIMUM_AMOUNT;
+        }
+
+        winAmount = (amount - houseEdge - jackpotFee) * modulo / rollUnder;
     }
 
     function settleBet() external onlyCroupier {
